01d6e2f9f01951681442ac2c7117f0f796875b32
Cristian Negrau <cristian.negrau@protonmail.com>
2021-07-13 17:48:37 +0300

    resolved second exercise

---
+++ numbers.py
@@ -0,0 +1,9 @@
+# Exercitiu 2:
+# Generati primele 50 de numere in ordine descrescatoare
+
+def reverse_list():
+  list = [i for i in range(1, 51)]
+  list.reverse()
+  return list
+
+print(reverse_list())
