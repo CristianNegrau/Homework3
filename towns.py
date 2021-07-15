a9674fc3a4f2da24fe2796e5a442285a601536da
Cristian Negrau <cristian.negrau@protonmail.com>
2021-07-15 09:58:34 +0300

    resolved ninth exercise

---
+++ towns.py
@@ -0,0 +1,7 @@
+# Exercitiu 9
+list = ["Timisoara", "Oradea", "Bucuresti", "Brasov", "Constanta", "Cluj"]
+# Puneti prefixul "oras" la fiecare element din lista de mai sus
+
+new_list = ['oras' + i for i in list]
+
+print(new_list)
